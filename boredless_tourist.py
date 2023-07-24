e117b9dc5bb9f76567984cdf9164a97b9e467b6f
natlmartin <nat.louise.martin@gmail.com>
2023-07-24 18:12:26 +0100

    Add get_traveler_location
    Add logic to find traveler destinations and convert to internal data

---
+++ boredless_tourist.py
@@ -0,0 +1,19 @@
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+# Find index of traveler destination
+def get_destination_index(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
+
+# print(get_destination_index("Hyderabad, India"))
+
+# get index of traveler destination
+def get_traveler_location(traveler):
+    traveler_destination = test_traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
